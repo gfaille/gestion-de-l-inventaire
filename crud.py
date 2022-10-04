165a293fe651693069adbfdbfb3b86631ca1a630
benjamin-seneca <benjaminseneca@gmail.com>
2022-10-04 14:11:23 +0200

    Ajout de la fonction création utilisateur

--- crud.py
+++ crud.py
@@ -18,3 +18,11 @@ curseur.execute('''
 
 connexion.commit()
 connexion.close()
+
+# creation d'une fonction pour creer un utilisateur (role, prenom, nom, mdp)
+def create_user(role, prenom, nom, mdp):
+    connexion = sqlite3.connect('bdd.db')
+    curseur = connexion.cursor()
+    curseur.execute("INSERT INTO utilisateur VALUES (?, ?, ?, ?, ?)", (None, role, prenom, nom, mdp))
+    connexion.commit()
+    connexion.close()
