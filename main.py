c6f72dd7509b2ff2f3b7ca1fee87406c09e239ea
gfaille <zerteducon@gmail.com>
2022-10-11 21:18:42 +0200

    ajout de l'accueil de l'admin, création de pc, assignation de pc a utilisateur

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from signal import pause
 import fonction
 import crud
 import os
+import time
 
-Quitter = False
+quitter = True
 
-while True :
+while quitter :
 
     # écran d'inscription / connexion
     os.system("clear")
-    print("taper (1) pour s'inscrire\n taper (2) pour se connecter")
+    print(" taper (1) pour s'inscrire\n taper (2) pour se connecter \n taper (3) pour quitter")
 
     commande = int(input())
 
@@ -33,15 +33,44 @@ while True :
                 while True :
                     
                     print("Statistique : ")
-                    print("Nombre de pc en prêts : " + crud.calcul_pc())
-                    print("Nombre de ticket en cours : " + crud.ticket_en_cours("en cours"))
-                    print("Nombre de ticket terminés : " + crud.ticket_en_cours("terminé"))
+                    print("Nombre de pc en prêts : ", crud.calcul_pc())
+                    print("Nombre de ticket en cours : ", crud.ticket_en_cours("en cours"))
+                    print("Nombre de ticket terminés : ", crud.ticket_en_cours("terminé"))
+                    
+                    commande = input("P: crée un nouveau ordinateur,    A: ajoute un pc a un utilisateur,   \nT: voir vos tickets,    Q: quitter ")
+
+                    if commande == "p" :
+                        
+                        nb_pc = int(input("combien de pc voulez-vous crée"))
+
+                        for _ in range(nb_pc) :
+                            fonction.ajout_ordinnateur()
+                    
+                    elif commande == "a" :
+                        
+                        print(crud.select_user_admin(), crud.select_ordinateur())
+
+                        fonction.ajout_pc_user()
+                        
+                        time.sleep(20)
+                    
+                    elif commande == "t" :
+                        pass
+
+                    elif commande == "q" :
+                        break
+
+                    else :
+                        pass
             
             else :
 
                 #ecran accueil utilisateur
                 while True :
                     pass
+
+    elif commande == 3 :
+        quitter = False
 
     else :
         fonction.afficher_erreur()
